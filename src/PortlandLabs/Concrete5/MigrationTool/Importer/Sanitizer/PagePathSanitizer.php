689e2672c13d5dd1d146db7e0c1b54a7cb0ee28a
Michele Locati <michele@locati.it>
2025-01-17 15:16:16 +0100

    Improve PagePathSanitizer

--- src/PortlandLabs/Concrete5/MigrationTool/Importer/Sanitizer/PagePathSanitizer.php
+++ src/PortlandLabs/Concrete5/MigrationTool/Importer/Sanitizer/PagePathSanitizer.php
@@ -5,17 +5,26 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
 class PagePathSanitizer
 {
+    /**
+     * @param string|\SimpleXMLElement|null $path
+     *
+     * @return string
+     */
     public function sanitize($path)
     {
-        $parts = explode('/', $path);
-        $full = '';
-        foreach ($parts as $part) {
-            $txt = \Core::make('helper/text');
-            $part = $txt->slugSafeString($part);
-            $part = str_replace('-', \Config::get('concrete.seo.page_path_separator'), $part);
-            $full .= $part . '/';
+        $parts = preg_split('{/}', (string) $path, -1, PREG_SPLIT_NO_EMPTY);
+        if ($parts === []) {
+            return '';
         }
+        $txt = \Core::make('helper/text');
+        $pagePathSeparator = \Config::get('concrete.seo.page_path_separator');
+        $parts = array_map(
+            static function ($part) use ($txt, $pagePathSeparator) {
+                return str_replace('-', $pagePathSeparator, $txt->slugSafeString($part));
+            },
+            $parts
+        );
 
-        return rtrim($full, '/');
+        return implode('/', $parts);
     }
 }
