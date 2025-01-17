53fc01d6db4616ea6e88ee75efde6c72cb2d2f79
Michele Locati <michele@locati.it>
2025-01-17 15:17:07 +0100

    Skip some validations for page aliases/external links

--- src/PortlandLabs/Concrete5/MigrationTool/Batch/Validator/Pipeline/Stage/ValidatePageTemplatesStage.php
+++ src/PortlandLabs/Concrete5/MigrationTool/Batch/Validator/Pipeline/Stage/ValidatePageTemplatesStage.php
@@ -12,22 +12,31 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
 class ValidatePageTemplatesStage implements StageInterface
 {
+    /**
+     * @param \PortlandLabs\Concrete5\MigrationTool\Batch\Validator\ValidatorResult $result
+     *
+     * @return \PortlandLabs\Concrete5\MigrationTool\Batch\Validator\ValidatorResult
+     */
     public function __invoke($result)
     {
         $subject = $result->getSubject();
         $batch = $subject->getBatch();
         $page = $subject->getObject();
-
-        if ($page->getTemplate()) {
-            $mapper = new PageTemplate();
-            $targetItemList = new TargetItemList($batch, $mapper);
-            $item = new Item($page->getTemplate());
-            $targetItem = $targetItemList->getSelectedTargetItem($item);
-            if ($targetItem instanceof UnmappedTargetItem) {
-                $result->getMessages()->add(
-                    new Message(t('Page template <strong>%s</strong> does not exist.', $item->getIdentifier()), Message::E_WARNING)
-                );
-            }
+        /** @var \PortlandLabs\Concrete5\MigrationTool\Entity\Import\Page $page */
+        if (in_array($page->getKind(), [$page::KIND_ALIAS, $page::KIND_EXTERNAL_LINK], true)) {
+            return $result;
+        }
+        if (!$page->getTemplate()) {
+            return $result;
+        }
+        $mapper = new PageTemplate();
+        $targetItemList = new TargetItemList($batch, $mapper);
+        $item = new Item($page->getTemplate());
+        $targetItem = $targetItemList->getSelectedTargetItem($item);
+        if ($targetItem instanceof UnmappedTargetItem) {
+            $result->getMessages()->add(
+                new Message(t('Page template <strong>%s</strong> does not exist.', $item->getIdentifier()), Message::E_WARNING)
+            );
         }
 
         return $result;
